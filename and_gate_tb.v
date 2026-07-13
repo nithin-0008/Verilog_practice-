58ee1676f91e5d03c815d415015e98f03cb1127b
nithin-0008 <nithinkumarpatnaik@gmail.com>
2026-07-14 00:16:18 +0530

    Create and_gate_tb.v
    testbench

---
+++ and_gate_tb.v
@@ -0,0 +1,15 @@
+and_gate uut(
+  .a(a),
+  .b(b),
+  .y(y)
+);
+initial begin
+  $display("a b |y");
+  $monitor("%b %b| %b",a,b,y);
+  a=0;b=0;
+  #10 a=0;b=1;
+   #10 a=1;b=0;
+  #10 a=1;b=1;
+  #10 $finish;
+end
+endmodule
